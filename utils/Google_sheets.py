678d27dac5c53483afe280fffc19f98aceb4dd1e
Nikita <cicikinnikita@gmail.com>
2024-08-01 15:30:07 +0300

    Add new feature

--- utils/Google_sheets.py
+++ utils/Google_sheets.py
@@ -13,52 +13,55 @@ sample_range_name_of_programming_circle = "Кружок Программиров
 sample_range_name_of_student_scientific_society = "Студенческое научное общество"
 sample_range_name_of_curators = "Кураторы ИАТЭ НИЯУ МИФИ"
 sample_range_name_of_council_of_dormitories = "Совет общежитий (СО)"
-def read_google_sheets (name_of_sheet):
-  sample_range_name=name_of_sheet + "!A2:B50"
-  connected=False
-  creds = None
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", scopes)
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", scopes
-      )
-      creds = flow.run_local_server(port=0)
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
-  while connected==False:
-    try:
-      service = build("sheets", "v4", credentials=creds)
 
-      sheet = service.spreadsheets()
-      result = (
-          sheet.values()
-          .get(spreadsheetId=sample_spreadsheet_ID, range=sample_range_name)
-          .execute()
-      )
-    except HttpError as err:
-      connected=False
-    else:
-      connected=True
-  values = result.get("values", None)
-  question=[]
-  answer=[]
-  if values is None:
-    return
-  for row in values:
-    question.append(row[0])
-    answer.append(row[1])
-    dictionary=dict(zip(question,answer))
-  return dictionary
-def understand_question(name_of_sheet,string_of_user):
-  dictionary=read_google_sheets (name_of_sheet)
-  questions = []
-  for i in dictionary:
-    questions.append(i)
-  a = process.extractOne(string_of_user, questions)
-  unswer=dictionary[a[0]]
-  return unswer
 
+def read_google_sheets(name_of_sheet):
+    sample_range_name = name_of_sheet + "!A2:B50"
+    connected = False
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", scopes)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", scopes
+            )
+            creds = flow.run_local_server(port=0)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    while connected == False:
+        try:
+            service = build("sheets", "v4", credentials=creds)
+
+            sheet = service.spreadsheets()
+            result = (
+                sheet.values()
+                .get(spreadsheetId=sample_spreadsheet_ID, range=sample_range_name)
+                .execute()
+            )
+        except HttpError as err:
+            connected = False
+        else:
+            connected = True
+    values = result.get("values", None)
+    question = []
+    answer = []
+    if values is None:
+        return
+    for row in values:
+        question.append(row[0])
+        answer.append(row[1])
+        dictionary = dict(zip(question, answer))
+    return dictionary
+
+
+def understand_question(dictionary, user_question):
+    questions = list(dictionary.keys())
+    appropriate_question = process.extractOne(user_question, questions)
+    if appropriate_question[1] > 50:
+        unswer = dictionary[appropriate_question[0]]
+    else:
+        unswer = "Извините, мы не можем найти что-то похожее на ваш вопрос."
+    return unswer
